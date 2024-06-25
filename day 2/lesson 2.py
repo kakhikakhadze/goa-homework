3700bbeae3727a4a42ed604a06fb13832ca11131
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-06-25 15:28:31 +0400

    first commit

---
+++ day 2/lesson 2.py
@@ -0,0 +1,23 @@
+name="kaxi"
+
+print(name)
+
+#name_არი ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"nika"_არის ცვლადისთვის მნიშვნელობა
+
+#print_ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+#name_(str)ტიპის ცვლადი
+#age_(int)მთელი რიცხვი
+#height_(float)(ათწილადი)ტიპის ცვლადი
+#boolean_(bool)ტიპის ცვლადი
+
+knows_programing = True
+is_ugly = false
+
+print(type(age))  (int)
+print(type(name))  (str)
+print(type(surname))  (str)
+print(type(height))  (float)
+print(type(knows programing))  (bool)
