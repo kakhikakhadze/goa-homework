c99a270384d2366b67dddf04016c71f632961e9b
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-07-05 21:15:53 +0400

    second commit

--- day 2/lesson 2.py
+++ day 2/lesson 2.py
@@ -3,4 +3,4 @@ surname = "kaxadze"
 age = 15
 height=1.80
 
-print("me var " + name+". chemi gvaria " + surname + ". me var" +" "+str(age)+" wlis" + ". me simaglesi var" +" "+str(height))
+# print("me var " + name+". chemi gvaria " + surname 
