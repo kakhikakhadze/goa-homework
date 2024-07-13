6487004e762bb83a97602963ebb38def1a649a5b
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-07-13 15:53:04 +0400

    second commit

---
+++ day 11/lesson 1/lesson 1.py
@@ -0,0 +1,22 @@
+# #დავალება 1
+
+# #a = int(input("enter a:"))
+# #b = int(input("enter b:"))
+
+# #print(a * b)
+
+# #დავალება 2
+
+# users = int(input("enter your users age "))
+
+# print(users+10)
+
+# #დავალება 3
+
+# name = "kaxi"
+# age = 15
+# height = 1.80
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
