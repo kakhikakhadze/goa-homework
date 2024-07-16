2550cced919017105be087298597c2d6d65077f4
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-07-16 14:57:09 +0400

    second commit

---
+++ day 11/homework/homework 1.py
@@ -0,0 +1,10 @@
+gmail = (input("enter your gmail"))
+sabuk = (input("enter your gmail"))
+
+print(gmail == sabuk )
+
+luka = (input("enter your age"))
+kakhi = (input("enter your age"))
+
+
+print(luka > kakhi)
