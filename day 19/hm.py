6876c070c3a6364534d249c254abd2a1641b6066
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-08-06 21:05:47 +0400

    second commit

---
+++ day 19/hm.py
@@ -0,0 +1,15 @@
+#num = int(input("enter your num"))
+#for i in range(4,20+1,2):
+#    print(i) 
+
+
+#i = 9
+#while i < 20:
+#    i = i + 1
+#    print(i)
+
+
+#i = 0
+#while i < 100:
+#    i = i + 2
+#    print(i)
