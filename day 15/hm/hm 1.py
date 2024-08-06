60664831d9064f76126e16a7035900dd805c9dd9
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-08-06 21:06:42 +0400

    second commit

---
+++ day 15/hm/hm 1.py
@@ -0,0 +1,26 @@
+#hm 1
+#for i in range(2,50,4):
+#    print(i)
+
+
+
+#hm 2
+#for i in range(10):
+#    print("goa")
+
+
+
+#hm 3
+#for i in range(1,10,2):
+#    print(i)
+
+#for i in range(2,11,2):
+#    print(i)
+
+
+#hm 4
+#num = int(input("guess number:"))
+#while num != 5:
+#    num = int(input("gues number:"))
+    
+    
