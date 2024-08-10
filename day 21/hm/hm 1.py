004946426792c17a597779033143b8155e4631d9
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-08-10 18:42:01 +0400

    second commit

---
+++ day 21/hm/hm 1.py
@@ -0,0 +1,47 @@
+#დავალება 1
+
+#num = int(input("guess number:"))
+#while num != 100:
+#    num = int(input("gues number:"))
+
+#დავალება 2
+
+#for i in range(100):
+#    if i % 3 == 0:
+#        print("fizz")
+#    elif i % 5 == 0:
+#        print("buzz")
+#    elif i % 3 == 0 and i % 5 == 0:
+#        print("fizzbuzz")
+#    else:
+#        print(i)
+
+#დავალება 3
+
+#kursi1 = input("shemoiyvanet pirveli kursis qula : ")
+#if int(kursi1) > 100 or int(kursi1) < 0:
+#    print("araswori qula")
+#kursi2 = input("shemoiyvanet meore kursis qula : ")
+#if int(kursi2) > 100 or int(kursi2) < 0:
+#    print("araswori qula")
+#kursi3 = input("shemoiyvanet mesame kursis qula : ")
+#if int(kursi3) > 100 or int(kursi3) < 0:
+#    print("araswori qula")
+#if ((int(kursi1) + int(kursi2) + int(kursi3)) / 3)>70:
+#    print("gamocda cabarebulia")
+#else:
+#    print("gamocda ar aris cabarebuli")
+
+#დავალება 4 
+
+martvismowmoba = input("seiyvanet tqveni asaki : ")
+if int(martvismowmoba) > 18 or int(martvismowmoba) < 0:
+    print("araswori wlovanoba")
+manqanisweli = input(" seiyvanet manqanis weli : ")
+if int(manqanisweli) > 2005 or int(manqanisweli) < 2024:
+    print("araswori manqanis weli")
+ramdenxnianigamocdilebagaqvt = input("seiyvanet tqveni martvis gamocdileba : ")
+if int(ramdenxnianigamocdilebagaqvt) < 1:
+    print("sakmarisi gamocdileba ar gaqvt")
+else :
+    print("gamocda ar aris cabarebuli")
