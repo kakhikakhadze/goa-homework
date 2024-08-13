6bead4aff3c4cdba816dbb7b6553dda1e5b55526
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-08-13 15:13:14 +0400

    second commit

---
+++ day 23/hm/lists .py
@@ -0,0 +1,38 @@
+# დავალება 1
+
+#data = ['hello' , 'how' , 200 , 102.2 , "true" , "false" , 'tbilisi' , 'num 1' , 'energy' , 'angry']
+
+#data[0] = 'hi'
+#data[2] = '203'
+#data[6] = 'batumi'
+#data[5] = 'true'
+#data[7] = 'num 2'
+
+#print(data)
+
+# დავალება 2
+
+#        0  1  2  3  4  5  6  7  8  9  
+#nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+
+#print(nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7] + nums[8] + nums[9]) # 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10'
+
+# დავალება 3
+
+#txt = [10 , 25 , 18 , 23 , 36]
+
+#print(txt[0] * 2)
+#print(txt[1] * 2)
+#print(txt[2] * 2)
+#print(txt[3] * 2)
+#print(txt[4] * 2)
+
+#დავალება 4
+
+txt = [20 , 43 , 39 , 21 , 63]
+
+print(txt[0] * txt[4])
+print(txt[1] * txt[3])
+print(txt[2] * txt[1])
+print(txt[3] * txt[2])
+print(txt[4] * txt[0])
