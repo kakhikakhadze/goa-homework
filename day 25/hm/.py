57b6371730e9fcbcc00e1b1f813ed7a7fa7a9da9
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-08-17 15:19:41 +0400

    second commit

---
+++ day 25/hm/.py
@@ -0,0 +1,27 @@
+#hm 1
+
+#data = [5, 2 , 3]
+#data[0] = 1
+#print(data)
+
+#hm 2
+
+#data = ["fortnite","pubg","counter"]
+
+#data[0] = ["html"]
+#data[1] = ["css"]
+#data[2] = ["javascript"]
+
+#print(data)
+
+#hm 3
+
+#list = [1,2,3,4,5]
+
+#print(list[-1]) , print(list[-2]) , print(list[-3]) , print(list[-4]) , print(list[-5])
+
+#hm 4
+
+#list = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+
+#print(list[1:5:3])
