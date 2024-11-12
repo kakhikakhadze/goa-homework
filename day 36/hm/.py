401ebd4aa1c1f726674468f75c1b97c13b5156b1
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-11-13 02:03:50 +0400

    second commit

---
+++ day 36/hm/.py
@@ -0,0 +1,88 @@
+# 1
+#balance = int(input("please enter your balance:"))
+#withdraw = int(input("please enter your withdraw:"))
+
+#print(balance + withdraw)
+# 2
+#num = int(input("enter num :"))
+
+#def num1():
+#    if num % 2 == 0 :
+#        print(str(num) + " luwia")
+#    else:print("kenti")
+
+#    num1()
+# 3
+#num = int(input("enter num: "))
+
+#def num1():
+#    if num > 0 :
+#        print(str(num) + "dadebitia")
+#    else:
+#        print("uaryofitia")
+
+#num1()
+
+# 4
+
+#list = [12,3,4,5,6,7,8,9,0]
+
+#def num1():
+#    for i in list:
+#        print(i)
+
+#num1()     
+
+
+# 5
+
+#num = 10
+#num2 = 20
+
+#def num1():
+#    if num > num2:
+#        print(num)
+#    elif num2 > num:
+#        print(num2)
+#    else:
+#        print(str(num) + str(num2))  
+
+#num1() 
+
+# 6
+
+# num = 20
+# num2 = 10
+
+#    def num1():
+#        if num > num2:
+#    print(num2)     
+#        elif num2 > num:
+#    print(num)
+#num1()
+
+# 7
+
+#num1 = "100"
+#num2 = "200"
+#def amocanebi():
+#    print(int(num1) - int(num2))
+
+#amocanebi() 
+
+# 8
+
+# list = [1,2,3,4,5,6,7,8,9,0]
+
+# def num():
+#     printlist[0])
+# num1()
+
+# 9
+
+#num = int(input("name : "))
+#def num1():
+#    print(num ** 2)
+#    num1()
+
+
