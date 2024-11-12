0f6ea62882fac2e1bb6af75cfba1be5433a9e838
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-11-13 02:02:24 +0400

    second commit

---
+++ day 34/hm/.py
@@ -0,0 +1,50 @@
+# 1
+
+#num = int(input("price : "))
+#num1 = int(input("price : "))
+#def num1():
+#    print(num + num1)
+#num1()
+
+# 2
+
+#num = (input("total : "))
+#def num1():
+#    print(len(num))
+#num1()
+
+# 3
+
+#num = (input("price : "))
+#def num1():
+#    print(len(num))
+#num1()    
+
+# 4
+
+#num = int(input("price : "))
+#num1 = int(input("price : "))
+
+#if num > num1:
+#    print(num)
+#elif num1 > num:
+#    print(num1)
+#else:
+#    print("operation do not work")
+
+# 5
+
+#user = int(input("nums :"))
+
+#if user > 10 :
+#    print("operation do not work")
+#elif user < 10 :
+#    print("correct")
+#else:
+#    print("its not correct")
+
+
+
+
+
+
