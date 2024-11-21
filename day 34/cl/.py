e14651ae39131c003bda87119b9a3db0c868890c
kakhikakhadze <kakhikakhadze5@gmail.com>
2024-11-21 15:42:07 +0400

    second commit

---
+++ day 34/cl/.py
@@ -0,0 +1,8 @@
+def give_me_water_pls():
+    print("წავედი")
+    print("ავიღე ჭიქა")
+    print("ჩავასხი წყალი")
+    print("დავლიე წყალი")
+    print("ჩავდე ჭიქა ნიჟარაში")
+
+give_me_water_pls()
